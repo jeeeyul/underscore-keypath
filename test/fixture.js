951e4cc99913a6f2c87300bf9cbf1a79c8cc2c81
Jeeeyul <Jeeeyul@gmail.com>
2014-02-07 19:34:55 +0900

    adding fixture for test cases

---
+++ test/fixture.js
@@ -0,0 +1,31 @@
+function Person(name, age){
+	this._name = name;
+	this._age = age;
+	this._male = true;
+};
+
+Person.prototype.getName = function(){
+	return this._name;
+};
+
+Person.prototype.setName = function(newName){
+	return this._name = newName;
+};
+
+Person.prototype.getAge = function(){
+	return this._age;
+};
+
+Person.prototype.setAge = function(newAge){
+	return this._age = newAge;
+};
+
+Person.prototype.isMale = function(){
+	return this._male;
+};
+
+Person.prototype.setMail = function(newMale){
+	return this._male = newMale;
+};
+
+exports.Person = Person;
