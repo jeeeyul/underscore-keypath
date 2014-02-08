1b611999e711720bbd6be74c67456fa72b7853b0
Jeeeyul <jeeeyul@gmail.com>
2014-02-08 19:15:21 +0900

    adds assertion to return value of setValueForKeyPath

--- test/setValueForKeyPath.js
+++ test/setValueForKeyPath.js
@@ -11,14 +11,16 @@ var fixture = {
 describe("setValueForKeyPath", function(){
 	describe("set plain property", function(){
 		it("plain property must be updated", function(){
-			_(fixture).setValueForKeyPath("foo.alias", "FOO");
+			_(fixture).setValueForKeyPath("foo.alias", "FOO")
+				.should.be.exactly("FOO");
 			fixture.foo.alias.should.be.exactly("FOO");
 		});
 	});
 
 	describe("set property by setter", function(){
 		it("what if there is setter for given keypath, it must be called", function(){
-			_(fixture).setValueForKeyPath("bar.age", 99);
+			_(fixture).setValueForKeyPath("bar.age", 99)
+				.should.be.exactly(99);
 			fixture.bar._age.should.be.exactly(99);
 		});
 	});
