9c77838b4e885cff29a9c0985114b6d9ac1ee26b
Jeeeyul <Jeeeyul@gmail.com>
2014-02-10 13:41:54 +0900

    fixed

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe("Keypath mechanism for underscore");
 
-Package.on_use(function(where, api){
+Package.on_use(function(api, where){
 	api.use("underscore", ["server", "client"]);
 
 	api.add_files("lib/underscore-keypath.min.js", ["server", "client"]);
