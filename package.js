97958daa9494f03a738d3602e51691a90c3796c5
Jeeeyul <Jeeeyul@gmail.com>
2014-02-12 14:54:21 +0900

    mrt export was relocated.

--- package.js
+++ package.js
@@ -6,5 +6,5 @@ Package.on_use(function(api, where){
 	api.use("underscore", ["server", "client"]);
 
 	api.add_files("lib/underscore-keypath.min.js", ["server", "client"]);
-	api.add_files("lib/export-underscore-keypath.js", ["server", "client"]);
+	api.add_files("export-underscore-keypath.js", ["server", "client"]);
 });
