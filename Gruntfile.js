beb42997cbf52e065cd6bcf5062310f811983cda
Jeeeyul <Jeeeyul@gmail.com>
2014-02-12 13:36:00 +0900

    grunt test

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		mochaTest: {
+			testCases : ["test/*.js"],
+			options : {
+				reporter : "spec"
+			}
+		},
+
+		jshint : {
+			source : ["lib/underscore-keypath.js"]
+		},
+
+		uglify: {
+			options: {
+				sourceMap : function(filename){
+					return filename + ".map";
+				}
+			},
+			main : {
+				files : {
+					"lib/underscore-keypath.min.js" : "lib/underscore-keypath.js"
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks("grunt-mocha-test");
+
+	grunt.registerTask("default", ["jshint", "mochaTest:testCases"]);
+	grunt.registerTask("minify", "uglify:main");
+};
